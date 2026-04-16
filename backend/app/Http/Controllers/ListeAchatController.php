1bba4a46863c0974a2d11da19fefd48398b36850
Jessica <jessica.stpierregagne@gmail.com>
2026-04-16 15:48:57 -0400

    creation du model migration et controller de la listeAchats

---
+++ backend/app/Http/Controllers/ListeAchatController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListeAchat;
+use Illuminate\Http\Request;
+
+class ListeAchatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ListeAchat $listeAchat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ListeAchat $listeAchat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ListeAchat $listeAchat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ListeAchat $listeAchat)
+    {
+        //
+    }
+}
